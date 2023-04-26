e45b1e346198a5381f1848064966ee6eb3a6244e
mk4422 <muhammadali.rasuljonov223@gmail.com>
2023-04-26 10:36:07 +0500

    second commit

--- app/src/main/java/uz/muhammadali/mk/MainActivity.kt
+++ app/src/main/java/uz/muhammadali/mk/MainActivity.kt
@@ -1,11 +1,13 @@
 package uz.muhammadali.mk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import uz.muhammadali.lib_shared_preference.PrefTestClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        PrefTestClass.show(this)
     }
 }
---
+++ lib-shared-preference/src/main/java/uz/muhammadali/lib_shared_preference/PrefTestClass.kt
@@ -0,0 +1,10 @@
+package uz.muhammadali.lib_shared_preference
+
+import android.content.Context
+import android.widget.Toast
+
+object PrefTestClass {
+    fun show(context: Context) {
+        Toast.makeText(context, "YEAH SUCCESS", Toast.LENGTH_LONG).show()
+    }
+}
