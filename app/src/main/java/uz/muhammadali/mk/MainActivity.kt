ce93c6dd151d8d34460b39e2516b054d3953f989
mk4422 <muhammadali.rasuljonov223@gmail.com>
2023-04-26 10:13:30 +0500

    initial commit

---
+++ app/src/main/java/uz/muhammadali/mk/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.muhammadali.mk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
